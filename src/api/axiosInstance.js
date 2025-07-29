a6385b9e4cc758b93a0411be9e819ce3d2927453
ErDprogrammer <henryrichardemzy@gmail.com>
2025-07-29 13:52:21 +0100

    update axios instance baseURL configuration

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
 	// baseURL: 'http://172.20.10.2/cashtradeproApi/api/task/',
-	baseURL: 'http://172.19.18.22/backend/api/task/',
-	// baseURL: 'http://192.168.8.189/cashtradeproApi/api/task/',
-	// baseURL: 'http://192.168.1.117/backend/api/task/',
+	// baseURL: 'http://172.19.18.22/backend/api/task/',
+	// baseURL: 'http://192.168.1.238/cashtradeproApi/api/task/',
+	baseURL: 'http://192.168.1.117/backend/api/task/',
 	// baseURL: 'http://192.168.103.22/backend/api/task/',
 	// baseURL: 'https://api.cashtradepro.com/api/task/',
 	// withCredentials: true,
