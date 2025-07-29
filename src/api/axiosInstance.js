5cf526a098c6af8b969d321a4be067ef8359eed0
ErDprogrammer <henryrichardemzy@gmail.com>
2025-07-29 14:24:36 +0100

    updated

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,10 +1,14 @@
 // src/api/axiosInstance.js
 import axios from 'axios';
 
-const axiosInstance = axios.create({ 
-	// baseURL: 'http://192.168.1.212/backend/api/task/', 
-	baseURL: 'http://10.84.244.22/backend/api/task/', 
+const axiosInstance = axios.create({
+	// baseURL: 'http://172.20.10.2/cashtradeproApi/api/task/',
+	// baseURL: 'http://172.19.18.22/backend/api/task/',
+	baseURL: 'http://192.168.1.238/cashtradeproApi/api/task/',
+	// baseURL: 'http://192.168.1.117/backend/api/task/',
+	// baseURL: 'http://192.168.103.22/backend/api/task/',
 	// baseURL: 'https://api.cashtradepro.com/api/task/',
+	withCredentials: true,
 });
 
 axiosInstance.interceptors.request.use((config) => {
